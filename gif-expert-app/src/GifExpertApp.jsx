0499e4a876318f9e28389315acbbd0e42da38095
SRubiRosales <sharon_rosales98@hotmail.com>
2022-12-07 16:40:35 -0600

    Caterogiras en componente principal

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,22 @@
+import { useState } from "react";
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Marvel', 'DC']);
+  const onAddCategory = () => {
+    setCategories([...categories, 'Disney']);
+  }
   return (
     <>
-        <h1>Gif Expert App</h1>
+    {/* Título */}
+      <h1>Gif Expert App</h1>
+    {/* Input */}
+    {/* Listado de Gif */}
+    <button onClick={onAddCategory}>Agregar</button>
+    <ol>
+      { categories.map(category => {
+        return <li key={category}>{category}</li>
+      })}
+    </ol>
+    {/* Gif item */}
     </>
   )
 }
